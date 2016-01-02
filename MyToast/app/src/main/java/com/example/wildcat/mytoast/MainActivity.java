eb344f9b800509d36c11feb9b4503a8db9207d8c
Shannon Rhodes <srhodes91115@gmail.com>
2016-01-02 15:54:23 -0500

    Toast Project
    How to create a simple toast

---
+++ MyToast/app/src/main/java/com/example/wildcat/mytoast/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.wildcat.mytoast;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    private Button button;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new OnClickListener()
+        {
+           @Override
+           public void onClick(View arg0)
+            {
+                Toast.makeText(getApplicationContext(),editText.getText(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
